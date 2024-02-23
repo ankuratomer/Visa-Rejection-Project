aadc428fc01ff7cfcc1748bb795ef571b0d3b98e
Ankur Choudhary <ankur.tomer@gmail.com>
2024-02-23 06:29:58 +0530

    Requirement done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us-visa",
+    version="0.0.0",
+    author="Ankur",
+    author_email="ankur.tomer@gmail.com",
+    packages=find_packages()
+) 
